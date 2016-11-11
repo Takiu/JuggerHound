eeaffa465a27f8ab42bedf953be008404a896666
Takiu <takunori.kasai@gmail.com>
2016-11-11 03:00:40 -0300

    Boss y enemigo ojo

---
+++ source/sprites/EneOjo.hx
@@ -0,0 +1,78 @@
+package sprites;
+
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxSprite;
+import flixel.FlxG;
+
+/**
+ * ...
+ * @author ...
+ */
+class EneOjo extends Enemies
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		loadGraphic(AssetPaths.Eyev1__png, true, 16, 16);
+		animation.add("Anim", [0,1, 2, 3, 4, 5, 6, 7], 20, true);
+		animation.play("Anim");
+		FlxG.watch.add(EneOjo, "dis");
+	}
+	
+	var time : Int = 0;
+	private static var dis : Int;
+	
+	public function Movimiento(pla : FlxSprite):Void 
+	{			
+		dis = Math.round(Math.sqrt(Math.pow(this.x + 8 - pla.x, 2) + Math.pow(this.y + 8 - pla.y - 17, 2)));
+		if (dis < 100)	
+		{
+			time = 0;
+			if (this.x >= pla.x && this.y <= pla.y)
+			{
+				this.x -= 0.5;
+				this.y += 0.5;
+			}
+			else if (this.x <= pla.x && this.y <= pla.y)
+			{
+				this.x += 0.5;
+				this.y += 0.5;
+			}
+			else if (this.x >= pla.x && this.y >= pla.y)
+			{
+				this.x -= 0.5;
+				this.y -= 0.5;
+			}
+			else if (this.x <= pla.x && this.y >= pla.y)
+			{
+				this.x += 0.5;
+				this.y -= 0.5;
+			}
+			if (this.x > pla.x){
+				flipX = false;
+			}
+			if (this.x < pla.x){
+				flipX = true;
+			}
+		}
+		else
+		{
+			if (flipX)
+			{
+				x++;
+			}
+			else
+			{
+				x--;
+			}
+			if (time == 40)
+			{
+				time = 0;
+				flipX = (flipX)?false:true;
+			}
+		}
+		time++;
+	}
+	
+}
